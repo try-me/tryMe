693a1e899a3b030858cc8919a6ad42c57ebb15d7
sdcaulley <stephanie.caulley@gmail.com>
2016-11-05 11:32:22 -0700

    working on objects

--- scripts/data/data.js
+++ scripts/data/data.js
@@ -1,28 +1,28 @@
-function User () {
-  userName: ,
-  passWord: ,
-  email: ,
-  badges: , //acheived badges
-  points: ,
-  challenges: , //recently done challenges does this have a timeframe?
-  location: , //current loc
-  can_challenge: ,
-  can_locate: ,
-  level: , //current level of comfort
-  activities: , //types of activities willing to do
-  streak: , //latest streak
-  challenged: , //have you been challenged?
-  daily_comp: , //is your daily challenge complete?
-  daily_challenge: , //todays challenge
-  best_streak: , //best streak ever
-  weight: ,
-  g_weight: ,
-  days: ,
-  g_days: ,
-  miles: ,
-  g_miles: ,
-  steps: ,
-  g_steps:
+function User (name, pwd, email, badges, points, challenges, location, activities, ) {
+  userName: this.name,
+  passWord: this.pwd,
+  email: this.email,
+  badges: this.badges, //acheived badges
+  points: this.points,
+  challenges: this.challenges, //recently done challenges does this have a timeframe?
+  location: this.location, //current loc
+  can_challenge: this.canChallenge,
+  can_locate: this.canLocate,
+  level: this.level, //current level of comfort
+  activities: this.activities, //types of activities willing to do
+  streak: this.streak, //latest streak
+  challenged: this.challenged, //have you been challenged?
+  daily_comp: this.dailyComp, //is your daily challenge complete?
+  daily_challenge: this.dailyChallenge, //todays challenge
+  best_streak: this.bestStreak, //best streak ever
+  weight: this.weight,
+  g_weight: this.gWeight,
+  days: this.days,
+  g_days: this.gDays,
+  miles: this.miles,
+  g_miles: this.gMiles,
+  steps: this.steps,
+  g_steps: this.gSteps
 };
 
 function Challenges () {
@@ -57,5 +57,5 @@ function Badges () {
   leader_bdg: ,
   steps_bdg: ,
   ghost_bdg: ,
-  days_bdg:    
+  days_bdg:
 }
--- scripts/model/model.js
+++ scripts/model/model.js
@@ -4,6 +4,8 @@ function addBadges() {
 
 function addPoints() {
   //when user finishes challenges/events update the points in object
+  //each goal = 50pts
+  //above and beyond - ie - goal 30 mins, you do 60 mins, 50 x 1.5
 };
 
 function addFriends() {
