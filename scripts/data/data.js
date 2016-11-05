e218a974ae03660cd8b12ae31b002304562c62f2
Alia Ward <aliaward@gmail.com>
2016-11-05 10:52:28 -0700

    added objects to js

--- scripts/data/data.js
+++ scripts/data/data.js
@@ -1,5 +1,61 @@
 function User () {
   userName: ,
   passWord: ,
-  email: 
+  email: ,
+  badges: , //acheived badges
+  points: ,
+  challenges: , //recently done challenges does this have a timeframe?
+  location: , //current loc
+  can_challenge: ,
+  can_locate: ,
+  level: , //current level of comfort
+  activities: , //types of activities willing to do
+  streak: , //latest streak
+  challenged: , //have you been challenged?
+  daily_comp: , //is your daily challenge complete?
+  daily_challenge: , //todays challenge
+  best_streak: , //best streak ever
+  weight: ,
+  g_weight: ,
+  days: ,
+  g_days: ,
+  miles: ,
+  g_miles: ,
+  steps: ,
+  g_steps:
 };
+
+function Challenges () {
+  sun_low: ,
+  sun_med: ,
+  sun_high: ,
+  mon_low: ,
+  mon_med: ,
+  mon_high: ,
+  tues_low: ,
+  tues_med: ,
+  tues_high: ,
+  weds_low: ,
+  weds_med: ,
+  weds_high: ,
+  thurs_low: ,
+  thurs_med: ,
+  thurs_high: ,
+  fri_low: ,
+  fri_med: ,
+  fri_high: ,
+  sat_low: ,
+  sat_med: ,
+  sat_high:
+}
+
+function Badges () {
+  start_bdg: ,
+  goal_bdg: ,
+  mile_bdg: ,
+  inspo_bdg: , //all given challenges and received challenges completed
+  leader_bdg: ,
+  steps_bdg: ,
+  ghost_bdg: ,
+  days_bdg:    
+}
